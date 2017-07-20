86350a30a869d08bfdc45cfc09159b37b418ee50
Rachid Calazans <rachidcalazans@gmail.com>
2017-07-19 22:05:15 -0300

    Update init command spec
    - add tests for #run method to verify the .chid.config content.
    
    Signed-off-by: Rachid Calazans <rachidcalazans@gmail.com>

--- spec/lib/chid/commands/init_spec.rb
+++ spec/lib/chid/commands/init_spec.rb
@@ -1,5 +1,64 @@
 require 'spec_helper'
 
-describe 'Init' do
+describe Chid::Commands::Init do
+
+  subject { Chid::Commands::Init.new(chid_config_path) }
+
+  let(:chid_config_path) { '' }
+
+  ## TODO: Extract for a shared context test
+  describe 'Documentations classes attributes' do
+    it('summary should not be nil') { expect(subject.class.summary).not_to be_nil }
+    it('description should not be nil') { expect(subject.class.description).not_to be_nil }
+    it('arguments should be empty') { expect(subject.class.arguments).to be_empty }
+  end
+
+  describe '#run' do
+    let(:chid_config_path) { File.join(File.dirname(__FILE__), '.chid.config') }
+    let(:file_content_configurations) { YAML.load_file(chid_config_path) }
+    let(:setup) {}
+
+    before do
+      setup
+      subject.run
+    end
+    after  { File.delete(chid_config_path) }
+
+    context 'When does not exist .chid.config file' do
+      let(:base_configuration) do
+        {
+          chid: {
+            workstations: {}
+          }
+        }
+      end
+
+      it 'Should contains the base configuration' do
+        expect(file_content_configurations).to eq base_configuration
+      end
+    end
+
+    context 'When does exist .chid.config file with some configuration' do
+      let(:existent_configurations) do
+        {
+          chid: {
+            workstations: {
+              base: 'vim'
+            }
+          }
+        }
+      end
+
+      let(:setup) do
+        File.open(chid_config_path, 'w') do |file|
+          YAML.dump(existent_configurations, file)
+        end
+      end
+
+      it 'Should remains the existent configuration' do
+        expect(file_content_configurations).to eq existent_configurations
+      end
+    end
+  end
 
 end
