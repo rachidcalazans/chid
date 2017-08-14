1ebc2688eb16dfb3de5c31e6e8e8233930097d19
Gaberu <gabrielrosadn@gmail.com>
2017-08-14 14:16:02 -0300

    Update gitflow commit class
    - implement method run to do his fuction
    
    Signed-off-by: Gaberu <gabrielrosadn@gmail.com>

--- lib/chid/commands/gitflow/commit.rb
+++ lib/chid/commands/gitflow/commit.rb
@@ -10,22 +10,32 @@ module Chid
         self.arguments = []
 
         def run
-          prompt = TTY::Prompt.new
+          @prompt = TTY::Prompt.new
           choices = ['Add', 'Remove','Update', 'Refactor','Fix']
-          result = prompt.select('Select the kind of commit:', choices)
-          puts 'This is a commit of type ' + result
-
-          puts 'Tell me the title of this commit'
-          print "> "
+          result = @prompt.select('Select the kind of commit:', choices)
+          puts 'Tell me the commit title'
+          print '> '
           commit_title = STDIN.gets.strip
+          @commit_lines = "\n"
+          add_commit_description
+          commit = "#{result} #{commit_title} \n #{@commit_lines}"
+          system("git commit -sm \"#{commit}\"")
+        end
 
+        def add_commit_description
           puts 'Tell me the commit description, 140 characteres per line'
           print "> "
-          commit_description = '- ' + STDIN.gets.strip
+          commit_description ="- #{STDIN.gets.strip} \n"
+          @commit_lines << commit_description
+          unless did_commit_finished?
+            add_commit_description
+          end
+        end
 
+        def did_commit_finished?
           answers = ['Yes','No']
-          result_tests_question = prompt.select('Have tests?', answers)
-          puts commit_description
+          result_description_finished = @prompt.select('Did you finish?', answers)
+          result_description_finished == 'Yes'
         end
 
       end
