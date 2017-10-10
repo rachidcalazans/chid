4b4d9deadf8874773ef6d789842520791a73bef1
Arthur Novaes <arthur.novaes@stant.com.br>
2017-10-10 14:18:49 -0300

    master Add instalation for gvim
    -
    
    Signed-off-by: Arthur Novaes <arthur.novaes@stant.com.br>

--- lib/chid/commands/installs/vim.rb
+++ lib/chid/commands/installs/vim.rb
@@ -5,11 +5,11 @@ module Chid
 
         command :'install vim'
 
-        self.summary = 'Install vim'
+        self.summary = 'Install vim and gvim'
         self.description = <<-DESC
 
 Usage:
-  $ chid install vim
+  $ chid install vim or chid install gvim
 
     For Linux users will install through apt-get
 
@@ -21,16 +21,40 @@ Usage:
         def run
           puts "\nInstalling vim..."
 
+          is_vim = do_vim?
+          is_gvim = do_gvim?
+
           ::Chid::on_linux do
-            system('sudo apt-get install vim')
+            system('sudo apt-get install vim') if is_vim
+            system('sudo apt-get install vim-gnome') if is_gvim
           end
 
           ::Chid::on_osx do
             system('brew install vim && brew install macvim')
           end
 
-          puts "\nVim installed successfully"
+          puts "\nVim installed successfully" if is_vim
+          puts "\nGvim installed successfully" if is_gvim
+
+          puts "\nNothing installed =(" unless is_vim || is_gvim
         end
+
+        def prompt
+          @prompt ||= TTY::Prompt.new
+        end
+
+        def do_vim?
+          answers = ['Yes','No']
+          should_install_vim = prompt.select('Install vim ?', answers)
+          should_install_vim == 'Yes'
+        end
+
+        def do_gvim?
+          answers = ['Yes','No']
+          should_install_gvim = prompt.select('Install gvim too?', answers)
+          should_install_gvim == 'Yes'
+        end
+
       end
     end
   end
