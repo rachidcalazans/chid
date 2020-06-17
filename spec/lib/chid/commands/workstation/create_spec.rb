eb043a9c53f7960a19f1cc3165daeda17b5376f5
Rachid Calazans <rachidcalazans@gmail.com>
2020-06-16 22:12:29 -0300

    Refactor Create Workstation Command tests
    - change the contexts sequence and setups
    
    Signed-off-by: Rachid Calazans <rachidcalazans@gmail.com>

--- spec/lib/chid/commands/workstation/create_spec.rb
+++ spec/lib/chid/commands/workstation/create_spec.rb
@@ -5,147 +5,85 @@ require 'spec_helper'
 # - [OK] Refactor test code
 # - [ ] Add feature to given options and arguments with workstation_name and app_names
 #     - chid workstation create -name tt -apps Safari,iTerm
-#     - [ ] Update test code to reuse setups and contexts
+#     - [OK] Update test code to reuse setups and contexts
 #     - [ ] Add the app_names option
 #
 
-
 describe ::Chid::Commands::Workstation::Create do
   describe '#run' do
-    let(:invoke_run) { Chid::Command.run(argv) }
-    let(:argv) do
-      commands = ['workstation', 'create']
-      commands.concat(options)
-      commands.compact!
-      commands
+    before do
+      reset_chid_config_file
+
+      set_chid_config_instance
+
+      set_plataform
+
+      set_get_option
+
+      set_multi_select_option
+
+      invoke_run
+    end
+    let(:reset_chid_config_file) do
+      template_data = {
+        chid: {
+          workstations: {},
+          tmux_templates: {}
+        }
+      }
+
+      File.open(chid_config.chid_config_path, 'w') do |file|
+        YAML.dump(template_data, file)
+      end
+    end
+    let(:set_get_option)           { allow(STDIN).to receive(:gets).and_return(workstation_name) }
+    let(:set_multi_select_option)  { allow_any_instance_of(TTY::Prompt).to receive(:multi_select).and_return(app_names) }
+    let(:set_chid_config_instance) { allow(::ChidConfig).to receive(:new).and_return chid_config }
+    let(:set_plataform)            { allow(::ChidConfig).to receive(:platform).and_return platform_name }
+
+    let(:invoke_run) do
+      argv = ['workstation', 'create']
+      argv.concat(options)
+      argv.compact!
+
+      Chid::Command.run(argv)
     end
 
-    context 'when NOT given any options' do
-      let(:options) { [] }
+    let(:chid_config)              { ::ChidConfig.new(home_base_path: "./spec/support") }
+    let(:load_chid_config_as_yaml) { YAML.load_file chid_config.chid_config_path }
 
-      context 'when the platform is osx' do
-        let(:platform_name) { 'Darwin' }
+    context 'when given a workstation name' do
+      let(:workstation_name) { 'tt5' }
 
-        context 'when given a workstation name' do
-          let(:workstation_name) { 'tt4 ' }
+      context 'when choose an app' do
+        let(:app_names) { %w[Safari] }
 
-          context 'when choose an app' do
-            let(:app_names) { %w[Safari] }
+        context 'when NOT given any options' do
+          let(:options) { [] }
 
-            let(:set_gets_options) do
-              allow(STDIN).to receive(:gets).and_return(workstation_name)
-              allow_any_instance_of(TTY::Prompt).to receive(:multi_select).and_return(app_names)
-            end
-
-            let(:chid_config)              { ::ChidConfig.new(home_base_path: "./spec/support") }
-            let(:set_chid_config_instance) { allow(::ChidConfig).to receive(:new).and_return chid_config }
-
-            let(:reset_chid_config_file) do
-              template_data = {
-                chid: {
-                  workstations: {},
-                  tmux_templates: {}
-                }
-              }
-
-              File.open(chid_config.chid_config_path, 'w') do |file|
-                YAML.dump(template_data, file)
-              end
-            end
-
-            let(:set_plataform) do
-              allow(::ChidConfig).to receive(:platform).and_return platform_name
-            end
-
-            before do
-              reset_chid_config_file
-
-              set_plataform
-
-              set_gets_options
-
-              set_chid_config_instance
-
-              invoke_run
-            end
-
-            let(:load_chid_config_as_yaml) { YAML.load_file chid_config.chid_config_path }
+          context 'when the platform is osx' do
+            let(:platform_name) { 'Darwin' }
 
             it 'add the new workstation name with choosen app on .chid.config file' do
-              yaml_file                 = load_chid_config_as_yaml
-              expected_workstation_name = :tt4
-              result                    = yaml_file.dig(:chid, :workstations, expected_workstation_name)
-              expected_app_names        = %w[Safari]
+              yaml_file = load_chid_config_as_yaml
+              result    = yaml_file.dig(:chid, :workstations, :tt5)
 
-              expect(result).to eq expected_app_names
+              expect(result).to eq %w[Safari]
             end
           end
         end
-      end
-    end
 
-#     - chid workstation create -name tt -apps Safari,iTerm
-      #   argv = ['init', '-option_1', 'value_for_option_1']
-      #
-      #   map_options_with_values(argv) #=> {'-option1' => ['value_for_option_1']}
-    context 'when given workstation name option' do
-      let(:options) { ['-name', workstation_name] }
+        context 'when given workstation name option' do
+          let(:options) { ['-name', workstation_name] }
 
-      context 'when the platform is osx' do
-        let(:platform_name) { 'Darwin' }
-
-        context 'when given a workstation name' do
-          let(:workstation_name) { 'tt4 ' }
-
-          context 'when choose an app' do
-            let(:app_names) { %w[Safari] }
-
-            let(:set_gets_options) do
-              # allow(STDIN).to receive(:gets).and_return(workstation_name)
-              allow_any_instance_of(TTY::Prompt).to receive(:multi_select).and_return(app_names)
-            end
-
-            let(:chid_config)              { ::ChidConfig.new(home_base_path: "./spec/support") }
-            let(:set_chid_config_instance) { allow(::ChidConfig).to receive(:new).and_return chid_config }
-
-            let(:reset_chid_config_file) do
-              template_data = {
-                chid: {
-                  workstations: {},
-                  tmux_templates: {}
-                }
-              }
-
-              File.open(chid_config.chid_config_path, 'w') do |file|
-                YAML.dump(template_data, file)
-              end
-            end
-
-            let(:set_plataform) do
-              allow(::ChidConfig).to receive(:platform).and_return platform_name
-            end
-
-            before do
-              reset_chid_config_file
-
-              set_plataform
-
-              set_gets_options
-
-              set_chid_config_instance
-
-              invoke_run
-            end
-
-            let(:load_chid_config_as_yaml) { YAML.load_file chid_config.chid_config_path }
+          context 'when the platform is osx' do
+            let(:platform_name) { 'Darwin' }
 
             it 'add the new workstation name with choosen app on .chid.config file' do
-              yaml_file                 = load_chid_config_as_yaml
-              expected_workstation_name = :tt4
-              result                    = yaml_file.dig(:chid, :workstations, expected_workstation_name)
-              expected_app_names        = %w[Safari]
+              yaml_file = load_chid_config_as_yaml
+              result    = yaml_file.dig(:chid, :workstations, :tt5)
 
-              expect(result).to eq expected_app_names
+              expect(result).to eq %w[Safari]
             end
           end
         end
