ea2fe5ee928af6eae4b87aeb2dabfaca28665b36
Rachid Calazans <rachidcalazans@gmail.com>
2020-08-11 21:38:30 -0300

    master Refactor AliasCreate command
    - refactor the comand and your tests
    
    Signed-off-by: Rachid Calazans <rachidcalazans@gmail.com>

--- lib/chid/commands/alias/create.rb
+++ lib/chid/commands/alias/create.rb
@@ -10,29 +10,41 @@ module Chid
 
 Usage:
 
-  # @todo - REDO USAGE DESC
   $ chid alias create -command {COMMAND} -alias {ALIAS}
+
+  Example:
+
+  $ chid alias create -command "chid workstation create" -alias cwo
 
         DESC
         self.arguments = ['-command', '-alias']
 
         def run
-          command     = options['-command'].first
-          alias_value = options['-alias'].first
-
-          bash_file = File.open(bashrc_path, 'a') do |file|
-            file.puts "alias #{alias_value}='#{command}'"
-          end
+          add_command_on_bashrc
 
           print "Please run: source ~/.bashrc".blue
-
-          true
         end
 
         private
 
+        def add_command_on_bashrc
+          File.open(bashrc_path, 'a') { |file| file.puts shell_command }
+        end
+
         def bashrc_path
           @bashrc_path ||= File.join(Dir.home, '.bashrc')
+        end
+
+        def shell_command
+          "alias #{alias_value}='#{command}'"
+        end
+
+        def alias_value
+          options['-alias'].first
+        end
+
+        def command
+          options['-command'].first
         end
       end
     end
--- spec/lib/chid/commands/alias/create_spec.rb
+++ spec/lib/chid/commands/alias/create_spec.rb
@@ -1,37 +1,39 @@
 require 'spec_helper'
 
 describe ::Chid::Commands::Alias::Create, '#run' do
+  let(:alias_command) { ['alias', 'create'] }
+
+  before do
+    reset_bashrc(bashrc_path)
+    override_bashrc_path
+    invoke_run
+  end
+
+  let(:bashrc_path) { File.join(Dir.pwd, 'spec/support/.bashrc') }
+  let(:override_bashrc_path) do
+    allow_any_instance_of(described_class).to receive(:bashrc_path)
+      .and_return(bashrc_path)
+  end
   let(:invoke_run) do
-    argv = ['alias', 'create']
-    argv.concat(options)
-    argv.compact!
+    argv = alias_command
+      .concat(arguments)
+      .compact
 
     Chid::Command.run(argv)
   end
 
-  before do
-    reset_bashrc
-
-    allow_any_instance_of(described_class).to receive(:bashrc_path)
-      .and_return(bashrc_path)
-
-    invoke_run
-  end
-  let(:bashrc_path) { File.join(Dir.pwd, 'spec/support/.bashrc') }
-
   context 'when given required arguments' do
-    let(:options) { ['-command', 'chid workstation open', '-alias', 'cwo'] }
+    let(:arguments) { ['-command', 'chid workstation open', '-alias', 'cwo'] }
 
     it 'add alias on .bashrc' do
-      bash_file = File.read(bashrc_path)
-      last_line = bash_file.split("\n").last
+      last_line = last_line_at_bashrc(bashrc_path)
 
       expect(last_line).to eql "alias cwo='chid workstation open'"
-      expect(invoke_run).to be == true
     end
+
   end
 
-  def reset_bashrc
+  def reset_bashrc(bashrc_path)
     bash_file = File.open(bashrc_path, 'w') do |file|
       str = <<~STR
         # Add RVM to PATH for scripting. Make sure this is the last PATH variable change.
@@ -44,4 +46,10 @@ describe ::Chid::Commands::Alias::Create, '#run' do
       file.puts str
     end
   end
+
+    def last_line_at_bashrc(bashrc_path)
+      File
+        .read(bashrc_path)
+        .split("\n").last
+    end
 end
